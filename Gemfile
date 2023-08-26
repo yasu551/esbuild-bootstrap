e443f1e3006da2076f85bf8c6ac963852bbec5b7
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2023-08-26 18:07:45 +0900

    add i18n_generator gem

--- Gemfile
+++ Gemfile
@@ -67,4 +67,6 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+
+  gem 'i18n_generators'
 end
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ Bundler.require(*Rails.groups)
 
 module EsbuildBootstrap
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
