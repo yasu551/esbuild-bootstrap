5bdecc68e1a410eea07effef0be8e3ba65d48897
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2023-09-19 01:21:25 +0900

    モーダルの後ろが動かないように

--- app/javascript/controllers/task_modal_controller.js
+++ app/javascript/controllers/task_modal_controller.js
@@ -5,11 +5,13 @@ export default class extends Controller {
 
   connect() {
     this.element.showModal();
+    document.body.style.overflow = 'hidden';
   }
 
   close(event) {
     if (event.target === this.element || event.target === this.closeButtonTarget) {
       this.element.close();
+      document.body.style.overflow = 'auto';
     }
   }
 }
